e5127f0b805ebceaddc1d181630465ba3511559f
tarikwaleed <tarikwaleed.tech@gmail.com>
2023-06-23 17:56:46 +0300

    Changes

--- scripts/.config/scripts/tmuxinator-sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp ~/.config/tmuxinator/*.yml ~/dotfiles/tmuxinator/.config/tmuxinator 2>/dev/null
-rm ~/.config/tmuxinator/*.yml
-cd ~/dotfiles
-stow tmuxinator
-cd -
-echo "Done!"
